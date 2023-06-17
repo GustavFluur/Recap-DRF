c29c36755337901d203831fb0f7f8121f3d8abf8
GustavFluur <gustavfluur@hotmail.com>
2023-06-17 11:32:31 +0000

    Added image_filter choice field and image validation

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,25 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+
+    image_filter_choices = [
+    ('_1977', '1977'), 
+    ('brannan', 'Brannan'),
+    ('earlybird', 'Earlybird'), 
+    ('hudson', 'Hudson'),
+    ('inkwell', 'Inkwell'), 
+    ('lofi', 'Lo-Fi'),
+    ('kelvin', 'Kelvin'), 
+    ('normal', 'Normal'),
+    ('nashville', 'Nashville'), 
+    ('rise', 'Rise'),
+    ('toaster', 'Toaster'), 
+    ('valencia', 'Valencia'),
+    ('walden', 'Walden'), 
+    ('xpro2', 'X-pro II')
+    
+    ]
+
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -11,7 +30,10 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to = 'images/', default= '../default_profile_bhjl12', blank=True
     )
-    
+    image_filter = models.CharField(
+        max_length =32, choices=image_filter_choices, default='normal'
+    )
+
     class Meta: 
         ordering = ['-created_at']
     def __str__(self):
