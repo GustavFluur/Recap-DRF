79cb66501c37c7178cef65e1ea2d1edf73c29972
GustavFluur <gustavfluur@hotmail.com>
2023-07-03 17:57:31 +0000

    Forgot to makemigrations & migrate

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -66,7 +66,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'pp5-drf.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', os.environ.get('ALLOWED_HOST'),]
 
 
 # Application definition
