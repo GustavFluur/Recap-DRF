b541b112c7954f5c16a458a5fbf05b5fa064fb51
GustavFluur <gustavfluur@hotmail.com>
2023-07-04 19:42:57 +0000

    rm env.py since no gitignore file don't exist

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
- 
-os.environ['CLOUDINARY_URL'] = "cloudinary://481435885787938:DtXxeigVOf0S2Fz8Nz0lHx786Lc@deudink05"
-os.environ['SECRET_KEY'] = "Ok"
-os.environ['DEV'] = '1'
-os.environ['DATABASE_URL'] = "postgres://agcbotnj:S0LbYXK-pzcjPfZV5FwAJeVXX9jo9QRE@tyke.db.elephantsql.com/agcbotnj"
