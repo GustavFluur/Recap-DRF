6590ba71a271b89ee980974124bd677b0a7978d7
GustavFluur <gustavfluur@hotmail.com>
2023-07-05 15:25:02 +0000

    Last commit

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -69,7 +69,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', os.environ.get('ALLOWED_HOST'),]
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
+    ]
 
 
 # Application definition
